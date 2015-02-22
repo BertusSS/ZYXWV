ed1621030875daaa6a0ad5e48974ffb893381867
AnubisYinepu <yinepuanubis@gmail.com>
2015-02-23 01:34:58 +0200

    Version 11

---
+++ Mods/aircraft/Impala-Mk2-Su27Cpt/Liveries/Impala-Mk2/Gannet/description.lua
@@ -0,0 +1,7 @@
+livery = {
+	{"mat13", 0, "Imp_fuse_T", false},
+	{"mat16", 0, "Imp_tailplane_T", false},
+	{"mat15", 0, "Imp_wing_left_T", false},
+	{"mat17", 0, "Imp_wing_right_T", false},
+}
+name = "Gannet"
