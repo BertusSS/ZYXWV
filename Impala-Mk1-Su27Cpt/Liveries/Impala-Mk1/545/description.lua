b4be4da408fab3108177a1836f0732bc171ea6e9
AnubisYinepu <yinepuanubis@gmail.com>
2015-02-22 12:08:56 +0200

    Changes

---
+++ Impala-Mk1-Su27Cpt/Liveries/Impala-Mk1/545/description.lua
@@ -0,0 +1,8 @@
+livery = {
+	{"mat13", 0, "Imp_fuse_T", true},
+	{"mat18", 0, "Imp_pilot_T", true},
+	{"mat16", 0, "Imp_tailplane_T", true},
+	{"mat15", 0, "Imp_wing_left_T", true},
+	{"mat17", 0, "Imp_wing_right_T", true},
+}
+name = "545"
---
+++ Impala-Mk1-Su27Cpt/Liveries/Impala-Mk1/Falcon5/description.lua
@@ -0,0 +1,8 @@
+livery = {
+	{"mat13", 0, "Imp_fuse_T_F5.bmp", false},
+	{"mat18", 0, "Imp_pilot_T_F5.bmp", false},
+	{"mat16", 0, "Imp_tailplane_T_F5.bmp", false},
+	{"mat15", 0, "Imp_wing_left_T_F5.bmp", false},
+	{"mat17", 0, "Imp_wing_right_T_F5.bmp", false},
+}
+name = "Falcon5"
